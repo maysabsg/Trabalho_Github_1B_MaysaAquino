b34234eb0a9a9681878efd773dc211b79465ac34
maysa195 <57270886+maysa195@users.noreply.github.com>
2019-11-19 17:46:35 -0300

    Update github.cpp
    Fiz duas funções e metade de outra.

--- github.cpp
+++ github.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
-
+#include<string>
+using namespace std;
 
 /*
 	Conta todas as letras 'letra' contidas em 'texto'
 */
-int contaLetras(char letra, string texto)
+int contaLetras( string texto)
 {
+    int qtd;
+    qtd=texto.length();
+    return qtd;
 }
 
 /*
@@ -13,6 +17,16 @@ int contaLetras(char letra, string texto)
 */
 void substituirLetra(char letra1,char letra2, string texto)
 {
+    int i;
+    int count=contaLetras(texto);
+    for(i=0;i<count; i++)
+    {
+        if(texto[i]==letra1)
+            texto[i]=letra2;
+        else
+            if(texto[i]==letra2)
+            texto[i]=letra1;
+    }
 }
 
 /*
@@ -20,6 +34,13 @@ void substituirLetra(char letra1,char letra2, string texto)
 */
 int contaPalavras(string palavra, string texto)
 {
+    int i, n;
+for(i=0;i<'.';i++)
+    if(texto[i]==palavra[i])
+    n++;
+return n;
+
+
 }
 
 /*
@@ -30,11 +51,35 @@ void substituirPalavras(string palavra1, string palavra2, string texto)
 }
 
 int main(){
+    int ai, ou, np;
+    string txt, palavra;
+    char letter1, letter2;
+	cout << "\nDigite texto. Termine seu texto com ponto final e digite 'terminar' para terminar: ";
+	getline(cin,txt);
+	while(txt!='r')
+	{
+	ai=contaLetras(txt);
 
-	cout << "Entre com o texto para fazer o teste:";
+	cout<<"\n"<<ai<<" letras nesse texto.";
 
+    cout<<"\nDigite uma letra: ";
+    cin>>letter1;
+    cout<<"\nDigite a outra letra: ";
+    cin>>letter2;
+    substituirLetra(letter1, letter2, txt);
 
+    cout<<"\nTexto com as letras substituidas: "<<txt;
 
+    cout<<"\nDigite uma palavra: ";
+    getline(cin,palavra);
+
+    np=contaPalavras(palavra, txt);
+
+    cout<<"\nA palavra "<<palavra<<" apareceu"<<np<<" vezes nesse texto";
+
+    cout << "\nDigite texto. Termine seu texto com ponto final e digite 'terminar' para terminar: ";
+	getline(cin,txt);
+	}
 
 	return 0;
 }
